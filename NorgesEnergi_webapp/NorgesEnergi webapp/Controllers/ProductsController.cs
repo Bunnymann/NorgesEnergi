7ffd1e2ecd24e9937e320bd1ccf50df06bf5301a
MariusFosseli <mariusfosseli@hotmail.com>
2018-01-22 10:23:27 +0100

    Made new Index file
    new index file for mian page

--- NorgesEnergi_webapp/NorgesEnergi webapp/Controllers/ProductsController.cs
+++ NorgesEnergi_webapp/NorgesEnergi webapp/Controllers/ProductsController.cs
@@ -14,7 +14,8 @@ namespace NorgesEnergi_webapp.Controllers
         {
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
+            new Product { Id = 4, Name = "TestItem", Category = "Test", Price = 0.99M  }
         };
 
         public IEnumerable<Product> GetAllProducts()
