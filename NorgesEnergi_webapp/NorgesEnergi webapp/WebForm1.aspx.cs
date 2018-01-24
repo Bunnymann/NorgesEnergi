f0c230840c604ad1d014ca3893fc04f900065f71
Bunnymann <Benjgb15@uia.no>
2018-01-24 11:42:54 +0100

    done some sheeeeit

--- NorgesEnergi_webapp/NorgesEnergi webapp/WebForm1.aspx.cs
+++ NorgesEnergi_webapp/NorgesEnergi webapp/WebForm1.aspx.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Drawing;
 
 
 namespace NorgesEnergi_webapp
@@ -13,35 +15,14 @@ namespace NorgesEnergi_webapp
     {
 
         string ConnectionString = "data source=telos.database.windows.net; database=Norges_Energi; user id=NorgesEnergi; password=Deterhemelig123";
-
+      
 
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            //Create the connection object
 
-            SqlConnection start_connection = new SqlConnection(ConnectionString);
-            try
-            {
-                // Pass the connection to the command object, so the command object knows on which
-                // connection to execute the command
-                SqlCommand start_cmd = new SqlCommand("SELECT hovedkategori FROM hoved_kat", start_connection);
-                // Open the connection. Otherwise you get a runtime error. An open connection is
-                // required to execute the command
-                start_connection.Open();
-                start_grid_view.DataSource = start_cmd.ExecuteReader();
-                start_grid_view.DataBind();
-            }
-            catch (Exception ex)
-            {
-                // Handle Exceptions, if any
-            }
-            finally
-            {
-                // The finally block is guarenteed to execute even if there is an exception. 
-                //  This ensures connections are always properly closed.
-                start_connection.Close();
-            }
+            
+  
         }
 
 
@@ -71,11 +52,10 @@ namespace NorgesEnergi_webapp
             }
         }
 
-        protected void Kundenummer_Text_KeyDown(object sender, EventArgs e)
-        {
 
-            Db_GridView.Rows[0].Cells[1].BorderColor= System.Drawing.Color.Blue; 
-        }
-    }
-    
+            protected void update_grid_color_Click(object sender, EventArgs e)
+            {
+            
+            }
+    }  
 }
