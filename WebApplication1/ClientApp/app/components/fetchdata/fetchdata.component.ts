d33fdfaa8719e732736e1a1469f6d5511af68977
MariusFosseli <mariusfosseli@hotmail.com>
2018-02-08 20:41:59 +0100

    dropdown list
    dropdown list with fixed input data

--- WebApplication1/ClientApp/app/components/fetchdata/fetchdata.component.ts
+++ WebApplication1/ClientApp/app/components/fetchdata/fetchdata.component.ts
@@ -7,6 +7,18 @@ import { Http } from '@angular/http';
     styleUrls: ['./admin.css']
 })
 export class FetchDataComponent {
+    
+    stage1 = [{ 'name': 'NOR' }, { 'name': 'SWE' }, { 'name': 'FIN' }];
+    selectedStage1 = this.stage1[0];
+
+    stage2 = [{ 'name': 'Option 1' }, { 'name': 'Option 2' }, { 'name': 'Option 3' }];
+    selectedStage2 = this.stage2[0];
+
+    stage3 = [{ 'name': 'Option 1' }, { 'name': 'Option 2' }, { 'name': 'Option 3' }];
+    selectedStage3 = this.stage3[0];
+
+    stage4 = [{ 'name': 'Option 1' }, { 'name': 'Option 2' }, { 'name': 'Option 3' }];
+    selectedStage4 = this.stage4[0];
 
     alertAdd() {
         alert("New helptext added!");
@@ -21,3 +33,4 @@ export class FetchDataComponent {
     }
 
 }
+
