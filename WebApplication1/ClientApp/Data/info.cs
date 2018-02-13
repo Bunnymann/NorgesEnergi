f723415a74b724b2f778da759bd082fa64697a36
MariusFosseli <mariusfosseli@hotmail.com>
2018-02-13 11:17:47 +0100

    added class info

--- WebApplication1/ClientApp/Data/info.cs
+++ WebApplication1/ClientApp/Data/info.cs
@@ -5,7 +5,15 @@ using System.Threading.Tasks;
 
 namespace WebApplication1.ClientApp.Data
 {
-    public class info
+    public class Info
     {
+        public int info_ID { get; set; }
+        public string stage1 { get; set; }
+        public string stage2 { get; set; }
+        public string stage3 { get; set; }
+        public string stage4 { get; set; }
+        public string helptext_sum { get; set; }
+        public string helptext_full { get; set; }
+        public string helptext_header { get; set; }
     }
 }
