5bc6aaa3f8f44e69bb272fb6633cabc7113d3833
MariusFosseli <mariusfosseli@hotmail.com>
2018-02-13 11:14:43 +0100

    Revert "Added class info"
    This reverts commit 41f827d3cdf483d88c9f89b9e2da0fd570ccc6fb.

--- WebApplication1/ClientApp/Data/info.cs
+++ WebApplication1/ClientApp/Data/info.cs
@@ -5,15 +5,7 @@ using System.Threading.Tasks;
 
 namespace WebApplication1.ClientApp.Data
 {
-    public class Info
+    public class info
     {
-        public int info_ID { get; set; }
-        public string stage1 { get; set; }
-        public string stage2 { get; set; }
-        public string stage3 { get; set; }
-        public string stage4 { get; set; }
-        public string helptext_sum { get; set; }
-        public string helptext_full { get; set; }
-        public string helptext_header { get; set; }
     }
 }
