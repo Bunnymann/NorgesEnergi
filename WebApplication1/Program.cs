b5ee665ff55f62a81864c557d022cbf6ec48f5b2
Bunnymann <Benjgb15@uia.no>
2018-02-14 12:42:06 +0100

    fixed merge

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -9,15 +9,7 @@ using System.Threading.Tasks;
 using Dapper;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-<<<<<<< HEAD
-using WebApplication1.ClientApp.Data;
-=======
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Dapper;
-using System.Configuration;
-using System.Data;
->>>>>>> 31760bc318330efa9594f1b6c262c29802d5b01a
+
 
 namespace WebApplication1
 {
@@ -26,29 +18,11 @@ namespace WebApplication1
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
-<<<<<<< HEAD
-            IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["TelosNE"].ConnectionString);
-            string SqlString = "SELECT * FROM dbo.Category;";
 
-
-            var ourCategory = (List<Category>)db.Query<Category>(SqlString);
-
-            foreach (var Category in ourCategory)
-            {
-                Console.WriteLine(new string('*', 20));
-                Console.WriteLine("\nCategory ID: " + Category.category_ID.ToString());
-                Console.WriteLine("First Name: " + Category.category_name);
-                Console.WriteLine(new string('*', 20));
-            }
-
-            Console.ReadLine();
-        }
-
-=======
             IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
         }
         
->>>>>>> 31760bc318330efa9594f1b6c262c29802d5b01a
+
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
