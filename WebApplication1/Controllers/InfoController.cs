4fc32f90273c870ef20edde368d007e99a12d9ed
Bunnymann <Benjgb15@uia.no>
2018-02-19 14:06:12 +0100

    added delete to info

--- WebApplication1/Controllers/InfoController.cs
+++ WebApplication1/Controllers/InfoController.cs
@@ -89,7 +89,35 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public ActionResult Edit(int id)
         {
-            var obj = conn.Query<Info>("select * FROM info WHERE Info_ID = @Info_ID", new { info_id = id });
+            var obj = conn.Query<Info>("select * FROM info WHERE Info_ID = @InfoID", new { infoID = id });
+
+            if (obj != null)
+            {
+                Info model = new Info();
+                model.stage1 = obj.FirstOrDefault().stage1;
+                model.stage2 = obj.FirstOrDefault().stage2;
+                model.stage3 = obj.FirstOrDefault().stage3;
+                model.stage4 = obj.FirstOrDefault().stage4;
+                model.helptext_sum = obj.FirstOrDefault().helptext_sum;
+                model.helptext_full = obj.FirstOrDefault().helptext_full;
+                model.helptext_header = obj.FirstOrDefault().helptext_header;
+                return View(model);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Info model, int id)
+        {
+            var obj = conn.Execute("update category set ([stage1]), ([stage2]), ([stage3]), ([stage4]), ([Helptext_sum]), ([helptext_full]), ([helptext_header]) VALUES (@stage01), (@stage02), (@stage03), (@stage04), (@helptextsum), (@helptextfull), (@helptextheader) where Info_ID = @InfoID", new { InfoID = id, stage01 = model.stage1, stage02 = model.stage2, stage03 = model.stage3, stage04 = model.stage4, helptextsum = model.helptext_sum, helptextfull = model.helptext_full, helptextheader = model.helptext_header });
+
+            return RedirectToAction("list");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var obj = conn.Query<Info>("select * from info where info_Id = @infoID", new { infoID = id });
 
             if (obj != null)
             {
@@ -108,9 +136,9 @@ namespace WebApplication1.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Info model, int id)
+        public ActionResult Delete(Info model, int id)
         {
-            var obj = conn.Execute("update category set ([stage1]), ([stage2]), ([stage3]), ([stage4]), ([Helptext_sum]), ([helptext_full]), ([helptext_header]) VALUES (@stage01), (@stage02), (@stage03), (@stage04), (@helptextsum), (@helptextfull), (@helptextheader) where Info_ID = @InfoID", new { InfoID = id, stage01 = model.stage1, stage02 = model.stage2, stage03 = model.stage3, stage04 = model.stage4, helptextsum = model.helptext_sum, helptextfull = model.helptext_full, helptextheader = model.helptext_header });
+            var obj = conn.Execute("delete from info where info_ID= @infoID", new { infoID = id });
 
             return RedirectToAction("list");
         }
