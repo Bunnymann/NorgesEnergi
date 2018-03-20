c101aff279cc3fb0d56a7e8bcfbb4c67f55caac9
MariusFosseli <mariusfosseli@hotmail.com>
2018-03-20 12:07:12 +0100

    new views

--- API/Controllers/InfoController.cs
+++ API/Controllers/InfoController.cs
@@ -10,6 +10,7 @@ namespace WebApplication1.Controllers
 {
     public class InfoController : Controller
     {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["TelosNE"].ToString());
 
         public ActionResult StagesDDL()
         {
@@ -54,5 +55,134 @@ namespace WebApplication1.Controllers
             var stageList = stages.stage3.Where(x => x.stage3_name.Contains(search)).ToList();
             return Json(stageList,JsonRequestBehavior.AllowGet);
         }
+
+        public ActionResult viewAll()
+        {
+            Norges_EnergiEntities info = new Norges_EnergiEntities();
+
+            return View();
+        }
+
+        //Get list from function "GetALL" under
+        public ActionResult List()
+        {
+            var obj = GetAll();
+            List<info> result = new List<info>();
+            if (obj != null)
+            {
+                foreach (var row in obj)
+                {
+                    info model = new info();
+                    model.info_ID = row.info_ID;
+                    model.stage1_ID = row.stage1_ID;
+                    model.stage2_ID = row.stage2_ID;
+                    model.stage3_ID = row.stage3_ID;
+                    model.stage4_ID = row.stage4_ID;
+                    result.Add(model);
+                }
+            }
+            return View(result);
+        }
+
+        //Get all values from table helptext ordered by helptext header name
+        public List<info> GetAll()
+        {
+            var obj = conn.Query<info>("Select * FROM info").OrderByDescending(u => u.info_ID).Take(10).ToList();
+            return obj;
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(info model)
+        {
+            var obj = InsertHelptext(model);
+            return RedirectToAction("list");
+        }
+        public bool InsertHelptext(info model)
+        {
+            int rowsAffected = conn.Execute("INSERT INTO info([stage1_ID], [stage2_ID], [stage3_ID], [stage4_ID]) VALUES (@stage1, @stage2, @stage3, @stage4)", new { stage1 = model.stage1_ID, stage2 = model.stage2_ID, stage3 = model.stage3_ID, stage4 = model.stage4_ID });
+            if (rowsAffected > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        /**
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var obj = conn.Query<info>("SELECT * FROM helptext WHERE helptext_ID =  @text_ID", new { text_ID = id });
+
+            if (obj != null)
+            {
+                info model = new info();
+                model.info_ID = obj.FirstOrDefault().info_ID;
+                model.stage1_ID = obj.FirstOrDefault().stage1_ID;
+                model.stage2_ID = obj.FirstOrDefault().stage2_ID;
+                model.stage3_ID = obj.FirstOrDefault().stage3_ID;
+                model.stage4_ID = obj.FirstOrDefault().stage4_ID;
+                return View(model);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var obj = conn.Query<info>("SELECT * FROM helptext WHERE helptext_ID = @textID", new { textID = id });
+
+            if (obj != null)
+            {
+                info model = new info();
+                model.info_ID = obj.FirstOrDefault().info_ID;
+                model.stage1_ID = obj.FirstOrDefault().stage1_ID;
+                model.stage2_ID = obj.FirstOrDefault().stage2_ID;
+                model.stage3_ID = obj.FirstOrDefault().stage3_ID;
+                model.stage4_ID = obj.FirstOrDefault().stage4_ID;
+                return View(model);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(info model, int id)
+        {
+            var obj = conn.Execute("UPDATE helptext SET [helptext_header] = @header, [helptext_short] = @text_short, [helptext_long] = @text_long WHERE helptext_ID = @helpID", new { helpID = id, header = model.helptext_header, text_short = model.helptext_short, text_long = model.helptext_long });
+
+            return RedirectToAction("list");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var obj = conn.Query<info>("SELECT * FROM helptext WHERE helptext_ID = @textID", new { textID = id });
+
+            if (obj != null)
+            {
+                info model = new info();
+                model.info_ID = obj.FirstOrDefault().info_ID;
+                model.stage1_ID = obj.FirstOrDefault().stage1_ID;
+                model.stage2_ID = obj.FirstOrDefault().stage2_ID;
+                model.stage3_ID = obj.FirstOrDefault().stage3_ID;
+                model.stage4_ID = obj.FirstOrDefault().stage4_ID;
+                return View(model);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(info model, int id)
+        {
+            var obj = conn.Execute("DELETE FROM helptext WHERE helptext_ID = @textID", new { textID = id });
+
+            return RedirectToAction("list");
+        }
+    
+     */
+
     }
 }
---
+++ API/Models/InfoViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class InfoViewModel
+    {
+        public int Stage1_ID { get; set; }
+        public string Stage1_name { get; set; }
+        public int Stage2_ID { get; set; }
+        public string Stage2_name { get; set; }
+        public int Stage3_ID { get; set; }
+        public string Stage3_name { get; set; }
+        public int Stage4_ID { get; set; }
+        public string Stage4_name { get; set; }
+        public int Helptext_ID { get; set; }
+        public string Helptext_header { get; set; }
+        public string Helptext_short { get; set; }
+        public string Helptext_long { get; set; }
+        public int Metatag_ID { get; set; }
+        public string Tag { get; set; }
+
+
+    }
+}
