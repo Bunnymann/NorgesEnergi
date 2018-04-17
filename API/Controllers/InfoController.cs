15d3350f01b84b1cf3eb97c21ea106113bf94e8a
MariusFosseli <mariusfosseli@hotmail.com>
2018-04-17 11:19:21 +0200

    metatag fix
    List now shows all metatags on one line, instead of one line for each tag.
    new method GetTags(int id)

--- API/Controllers/InfoController.cs
+++ API/Controllers/InfoController.cs
@@ -13,82 +13,85 @@ using System.Net;
 
 namespace WebApplication1.Controllers
 {
-public class InfoController : Controller
-{
-    SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["TelosNE"].ToString());
-    Norges_EnergiEntities db = new Norges_EnergiEntities();
-
-    public ActionResult DDLSearchStage3()
+    public class InfoController : Controller
     {
-        Norges_EnergiEntities stages = new Norges_EnergiEntities();
-        return View(stages.stage3.ToList());
-    }
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["TelosNE"].ToString());
+        Norges_EnergiEntities db = new Norges_EnergiEntities();
 
-    public List<InfoViewModel> GetFullList()
-    {
-        var obj = conn.Query<InfoViewModel>("SELECT info.info_ID, stage1.stage1_name, stage2.stage2_name, stage3.stage3_name, stage4.stage4_name, helptext.helptext_header, helptext.helptext_short, helptext.helptext_long, metatag.tag FROM info INNER JOIN stage1 ON info.stage1_ID = stage1.stage1_ID INNER JOIN stage2 ON info.stage2_ID = stage2.stage2_ID INNER JOIN stage3 ON info.stage3_ID = stage3.stage3_ID INNER JOIN stage4 ON info.stage4_ID = stage4.stage4_ID INNER JOIN helptext ON stage4.helptext_ID = helptext.helptext_ID INNER JOIN helptexttag ON helptext.helptext_ID = helptexttag.helptext_ID INNER JOIN metatag ON helptexttag.metatag_ID = metatag.metatag_ID;").OrderByDescending(u => u.stage1_name).ToList();
+        public ActionResult DDLSearchStage3()
+        {
+            Norges_EnergiEntities stages = new Norges_EnergiEntities();
+            return View(stages.stage3.ToList());
+        }
 
-        return obj;
-    }
+        public List<InfoViewModel> GetFullList()
+        {
+            //sql query does NOT ask for metatag.tag
+            //method getTags select tags
+            var obj = conn.Query<InfoViewModel>("SELECT info.info_ID, stage1.stage1_name, stage2.stage2_name, stage3.stage3_name, stage4.stage4_name, helptext.helptext_header, helptext.helptext_short, helptext.helptext_long FROM info INNER JOIN stage1 ON info.stage1_ID = stage1.stage1_ID INNER JOIN stage2 ON info.stage2_ID = stage2.stage2_ID INNER JOIN stage3 ON info.stage3_ID = stage3.stage3_ID INNER JOIN stage4 ON info.stage4_ID = stage4.stage4_ID INNER JOIN helptext ON stage4.helptext_ID = helptext.helptext_ID;" /*INNER JOIN helptexttag ON helptext.helptext_ID = helptexttag.helptext_ID INNER JOIN metatag ON helptexttag.metatag_ID = metatag.metatag_ID;"*/).OrderByDescending(u => u.stage1_name).ToList();
 
-    public ActionResult FullList()
-    {
-        var obj = GetFullList();
-        List<InfoViewModel> result = new List<InfoViewModel>();
+            return obj;
+        }
+
+        public ActionResult FullList()
+        {
+            var obj = GetFullList();
+            List<InfoViewModel> result = new List<InfoViewModel>();
             List<string> metatag = new List<string>();
-        if (obj != null)
-        {
-            foreach (var row in obj)
+            if (obj != null)
             {
-                InfoViewModel model = new InfoViewModel();
-                model.info_ID = row.info_ID;
-                model.stage1_name = row.stage1_name;
-                model.stage2_name = row.stage2_name;
-                model.stage3_name = row.stage3_name;
-                model.stage4_name = row.stage4_name;
-                model.helptext_header = row.helptext_header;
-                model.helptext_short = row.helptext_short;
-                model.helptext_long = row.helptext_long;
-                    model.tag = row.tag;
+                foreach (var row in obj)
+                {
+                    InfoViewModel model = new InfoViewModel();
+                    model.info_ID = row.info_ID;
+                    model.stage1_name = row.stage1_name;
+                    model.stage2_name = row.stage2_name;
+                    model.stage3_name = row.stage3_name;
+                    model.stage4_name = row.stage4_name;
+                    model.helptext_header = row.helptext_header;
+                    model.helptext_short = row.helptext_short;
+                    model.helptext_long = row.helptext_long;
+                    model.tag = GetTags(row.info_ID);
                     result.Add(model);
+                }
             }
+            return View(result);
         }
-        return View(result);
-    }
-    public ActionResult FullList_Admin()
-    {
-        Norges_EnergiEntities stages = new Norges_EnergiEntities();
-        var obj = GetFullList();
-        List<InfoViewModel> result = new List<InfoViewModel>();
-        if (obj != null)
+        
+        public ActionResult FullList_Admin()
         {
-            foreach (var row in obj)
+            Norges_EnergiEntities stages = new Norges_EnergiEntities();
+            var obj = GetFullList();
+            List<InfoViewModel> result = new List<InfoViewModel>();
+            if (obj != null)
             {
-                InfoViewModel model = new InfoViewModel();
-                model.info_ID = row.info_ID;
-                model.stage1_name = row.stage1_name;
-                model.stage2_name = row.stage2_name;
-                model.stage3_name = row.stage3_name;
-                model.stage4_name = row.stage4_name;
-                model.helptext_header = row.helptext_header;
-                model.helptext_short = row.helptext_short;
-                model.helptext_long = row.helptext_long;
-                    model.tag = row.tag;
+                foreach (var row in obj)
+                {
+                    InfoViewModel model = new InfoViewModel();
+                    model.info_ID = row.info_ID;
+                    model.stage1_name = row.stage1_name;
+                    model.stage2_name = row.stage2_name;
+                    model.stage3_name = row.stage3_name;
+                    model.stage4_name = row.stage4_name;
+                    model.helptext_header = row.helptext_header;
+                    model.helptext_short = row.helptext_short;
+                    model.helptext_long = row.helptext_long;
+                    model.tag = GetTags(row.info_ID);
 
-                result.Add(model);
+                    result.Add(model);
+                }
             }
+            return View(result);
         }
-        return View(result);
-    }
 
-    public ActionResult testCreate()
-    {
-        PopulateStage1DropDownList();
-        PopulateStage2DropDownList();
-        PopulateStage3DropDownList();
-        PopulateStage4DropDownList();
-        return View();
-    }
+        public ActionResult testCreate()
+        {
+            PopulateStage1DropDownList();
+            PopulateStage2DropDownList();
+            PopulateStage3DropDownList();
+            PopulateStage4DropDownList();
+            return View();
+        }
         [HttpPost]
         public ActionResult testcreate(InfoViewModel model)
         {
@@ -153,74 +156,75 @@ public class InfoController : Controller
             return RedirectToAction("FullList");
         }
 
-    private void PopulateStage1DropDownList(object stage1 = null)
-    {
-        var stage = from s in db.stage1
+        private void PopulateStage1DropDownList(object stage1 = null)
+        {
+            var stage = from s in db.stage1
                         orderby s.stage1_name
                         select s;
-        ViewBag.stage1_ID = new SelectList(stage, "stage1_ID", "stage1_name", stage1);
-    }
-
-    private void PopulateStage2DropDownList(object stage2 = null)
-    {
-        var stage = from s in db.stage2
-                    orderby s.stage2_name
-                    select s;
-        ViewBag.stage2_ID = new SelectList(stage, "stage2_ID", "stage2_name", stage2);
-    }
-
-    private void PopulateStage3DropDownList(object stage3 = null)
-    {
-        var stage = from s in db.stage3
-                    orderby s.stage3_name
-                    select s;
-        ViewBag.stage3_ID = new SelectList(stage, "stage3_ID", "stage3_name", stage3);
-    }
-
-    private void PopulateStage4DropDownList(object stage4 = null)
-    {
-        var stage = from s in db.stage4
-                    orderby s.stage4_name
-                    select s;
-        ViewBag.stage4_ID = new SelectList(stage, "stage4_ID", "stage4_name", stage4);
-    }
-
-    [HttpGet]
-    public ActionResult DeleteInfo(int id)
-    {
-        var obj = conn.Query<InfoViewModel>("SELECT info.info_ID, stage1.stage1_name, stage2.stage2_name, stage3.stage3_name, stage4.stage4_name, helptext.helptext_ID, helptext.helptext_header, helptext.helptext_short, helptext.helptext_long FROM info INNER JOIN stage1 ON info.stage1_ID = stage1.stage1_ID INNER JOIN stage2 ON info.stage2_ID = stage2.stage2_ID INNER JOIN stage3 ON info.stage3_ID = stage3.stage3_ID INNER JOIN stage4 ON info.stage4_ID = stage4.stage4_ID INNER JOIN helptext ON stage4.helptext_ID = helptext.helptext_ID WHERE info.info_ID = @infoID;", new { infoID = id });
-
-        if(obj != null)
-        {
-            InfoViewModel model = new InfoViewModel();
-            model.info_ID = obj.FirstOrDefault().info_ID;
-            model.stage1_name = obj.FirstOrDefault().stage1_name;
-            model.stage2_name = obj.FirstOrDefault().stage2_name;
-            model.stage3_name = obj.FirstOrDefault().stage3_name;
-            model.stage4_name = obj.FirstOrDefault().stage4_name;
-            model.helptext_ID = obj.FirstOrDefault().helptext_ID;
-            model.helptext_header = obj.FirstOrDefault().helptext_header;
-            model.helptext_short = obj.FirstOrDefault().helptext_short;
-            model.helptext_long = obj.FirstOrDefault().helptext_long;
-            return View(model);
+            ViewBag.stage1_ID = new SelectList(stage, "stage1_ID", "stage1_name", stage1);
         }
-        return View();
-    }
-        
-    [HttpPost]
-    public ActionResult DeleteInfo(info model, int id)
-    {
-        var obj = conn.Execute("DELETE FROM info WHERE info_ID = @infoID", new { infoID = id });
 
-        return RedirectToAction("FullList");
-    }
-     
+        private void PopulateStage2DropDownList(object stage2 = null)
+        {
+            var stage = from s in db.stage2
+                        orderby s.stage2_name
+                        select s;
+            ViewBag.stage2_ID = new SelectList(stage, "stage2_ID", "stage2_name", stage2);
+        }
+
+        private void PopulateStage3DropDownList(object stage3 = null)
+        {
+            var stage = from s in db.stage3
+                        orderby s.stage3_name
+                        select s;
+            ViewBag.stage3_ID = new SelectList(stage, "stage3_ID", "stage3_name", stage3);
+        }
+
+        private void PopulateStage4DropDownList(object stage4 = null)
+        {
+            var stage = from s in db.stage4
+                        orderby s.stage4_name
+                        select s;
+            ViewBag.stage4_ID = new SelectList(stage, "stage4_ID", "stage4_name", stage4);
+        }
+
+        [HttpGet]
+        public ActionResult DeleteInfo(int id)
+        {
+            var obj = conn.Query<InfoViewModel>("SELECT info.info_ID, stage1.stage1_name, stage2.stage2_name, stage3.stage3_name, stage4.stage4_name, helptext.helptext_ID, helptext.helptext_header, helptext.helptext_short, helptext.helptext_long FROM info INNER JOIN stage1 ON info.stage1_ID = stage1.stage1_ID INNER JOIN stage2 ON info.stage2_ID = stage2.stage2_ID INNER JOIN stage3 ON info.stage3_ID = stage3.stage3_ID INNER JOIN stage4 ON info.stage4_ID = stage4.stage4_ID INNER JOIN helptext ON stage4.helptext_ID = helptext.helptext_ID WHERE info.info_ID = @infoID;", new { infoID = id });
+
+            if (obj != null)
+            {
+                InfoViewModel model = new InfoViewModel();
+                model.info_ID = obj.FirstOrDefault().info_ID;
+                model.stage1_name = obj.FirstOrDefault().stage1_name;
+                model.stage2_name = obj.FirstOrDefault().stage2_name;
+                model.stage3_name = obj.FirstOrDefault().stage3_name;
+                model.stage4_name = obj.FirstOrDefault().stage4_name;
+                model.helptext_ID = obj.FirstOrDefault().helptext_ID;
+                model.helptext_header = obj.FirstOrDefault().helptext_header;
+                model.helptext_short = obj.FirstOrDefault().helptext_short;
+                model.helptext_long = obj.FirstOrDefault().helptext_long;
+                model.tag = GetTags(model.info_ID);
+                return View(model);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteInfo(info model, int id)
+        {
+            var obj = conn.Execute("DELETE FROM info WHERE info_ID = @infoID", new { infoID = id });
+
+            return RedirectToAction("FullList");
+        }
+
         [HttpGet]
         public ActionResult Edit(int id)
         {
-            var obj = conn.Query<InfoViewModel>("SELECT info.info_ID, stage1.stage1_name, stage2.stage2_name, stage3.stage3_name, stage4.stage4_name, helptext.helptext_header, helptext.helptext_short, helptext.helptext_long, metatag.tag FROM info INNER JOIN stage1 ON info.stage1_ID = stage1.stage1_ID INNER JOIN stage2 ON info.stage2_ID = stage2.stage2_ID INNER JOIN stage3 ON info.stage3_ID = stage3.stage3_ID INNER JOIN stage4 ON info.stage4_ID = stage4.stage4_ID INNER JOIN helptext ON stage4.helptext_ID = helptext.helptext_ID INNER JOIN helptexttag ON helptext.helptext_ID = helptexttag.helptext_ID INNER JOIN metatag ON helptexttag.metatag_ID = metatag.metatag_ID WHERE info.info_ID = @info;", new { info = id});
+            var obj = conn.Query<InfoViewModel>("SELECT info.info_ID, stage1.stage1_name, stage2.stage2_name, stage3.stage3_name, stage4.stage4_name, helptext.helptext_header, helptext.helptext_short, helptext.helptext_long, metatag.tag FROM info INNER JOIN stage1 ON info.stage1_ID = stage1.stage1_ID INNER JOIN stage2 ON info.stage2_ID = stage2.stage2_ID INNER JOIN stage3 ON info.stage3_ID = stage3.stage3_ID INNER JOIN stage4 ON info.stage4_ID = stage4.stage4_ID INNER JOIN helptext ON stage4.helptext_ID = helptext.helptext_ID INNER JOIN helptexttag ON helptext.helptext_ID = helptexttag.helptext_ID INNER JOIN metatag ON helptexttag.metatag_ID = metatag.metatag_ID WHERE info.info_ID = @info;", new { info = id });
 
-            if(obj != null)
+            if (obj != null)
             {
                 InfoViewModel model = new InfoViewModel();
                 model.stage1_name = obj.FirstOrDefault().stage1_name;
@@ -231,7 +235,7 @@ public class InfoController : Controller
                 model.helptext_short = obj.FirstOrDefault().helptext_short;
                 model.helptext_long = obj.FirstOrDefault().helptext_long;
                 model.tag = obj.FirstOrDefault().tag;
-                return View(model); 
+                return View(model);
             }
             return View();
         }
@@ -253,6 +257,25 @@ public class InfoController : Controller
             //var infoUpdate2 = conn.Execute("Update InfoViewModel set [stage1_name] = @name1, [stage2_name] = @name2, [stage3_name] = @name3, [stage4_name] = @name4, [helptext_header] = @headertxt, [helptext_short] = @shorttxt, [helptext_long] = @longtxt, [tag] = @metatag FROM InfoViewModel WHERE info_ID = @info", new { info = id, name1 = model.stage1_name, name2 = model.stage2_name, name3 = model.stage3_name, name4 = model.stage4_name, headertxt = model.helptext_header, shorttxt = model.helptext_short, longtxt = model.helptext_long, metatag = model.tag });
             return RedirectToAction("FullList");
         }*/
+        [HttpGet]
+        public string GetTags(int id)
+        {
+            List<string> tags = new List<string>();
+            var obj = conn.Query<metatag>("SELECT metatag.tag FROM info INNER JOIN stage1 ON info.stage1_ID = stage1.stage1_ID INNER JOIN stage2 ON info.stage2_ID = stage2.stage2_ID INNER JOIN stage3 ON info.stage3_ID = stage3.stage3_ID INNER JOIN stage4 ON info.stage4_ID = stage4.stage4_ID INNER JOIN helptext ON stage4.helptext_ID = helptext.helptext_ID INNER JOIN helptexttag ON helptext.helptext_ID = helptexttag.helptext_ID INNER JOIN metatag ON helptexttag.metatag_ID = metatag.metatag_ID where info_ID = @infoID;", new { infoID = id });
+            
+            if (obj != null)
+            {
+                foreach(var tag in obj)
+                {
+
+                    tags.Add(tag.tag.ToString());
+                }
+            }
+            string text = string.Join(", ", tags);
+            return text;
+        }
+        
+
 
 }
 }
